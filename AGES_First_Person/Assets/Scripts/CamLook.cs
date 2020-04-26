47a0068dc9737be8ea1841e0c250077dcd392eee
DutaiStudios <44248998+DutaiStudios@users.noreply.github.com>
2020-04-25 20:59:50 -0400

    Milestones 1-2
    First two milestones re-uploaded

---
+++ AGES_First_Person/Assets/Scripts/CamLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamLook : MonoBehaviour
+{
+
+    [SerializeField] float mouseSensativity;
+
+    [SerializeField] Transform PlayerB;
+
+    float xRot = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensativity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensativity * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90, 90);
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        PlayerB.Rotate(Vector3.up * mouseX);
+
+    }
+}
