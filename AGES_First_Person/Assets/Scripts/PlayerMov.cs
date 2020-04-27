717c9d21dd5c6cfffc3d45d4fad29052049bb772
DutaiStudios <44248998+DutaiStudios@users.noreply.github.com>
2020-04-27 03:02:15 -0400

    Asset Reintegration
    Multiple unity asset files reintegrated
    Music re-added
    Scripts brought in from previous project and repurposed.

--- AGES_First_Person/Assets/Scripts/PlayerMov.cs
+++ AGES_First_Person/Assets/Scripts/PlayerMov.cs
@@ -7,14 +7,53 @@ public class PlayerMov : MonoBehaviour
 
     [SerializeField] CharacterController cont;
     [SerializeField] float speed;
+    float truespeed;
+    float sprintspeed;
+    public bool canmove = true;
+    public bool cansprint = false;
 
+    private void Start()
+    {
+        truespeed = speed;
+        sprintspeed = speed * 2;
+
+    }
     void Update()
     {
+        if (canmove == true)
+        {
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         cont.Move(move * speed * Time.deltaTime);
+            sprint();
+        }
+
+        else if (canmove == false)
+        {
+            return;
+        }
+
+    }
+
+    void sprint()
+    {
+
+        if (cansprint == true)
+        {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            speed = sprintspeed;
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            speed = truespeed;
+        }
+        }
+
     }
 }
